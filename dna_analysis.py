9e2b6fd9738896150c6bc4c88653d204bcab7b4e
am0k <eleven.house@gmail.com>
2013-11-08 05:54:21 -0800

    completes hw4

--- dna_analysis.py
+++ dna_analysis.py
@@ -2,40 +2,21 @@
 # heruli08
 # Computer Science Foundations
 # Programming as a Way of Life
-# Homework 3: DNA analysis (Part 1)
+# Homework 3 & 4: DNA analysis (Part 1 & II)
 
-# This program reads DNA sequencer output and computes statistics, such as
-# the GC content. Run it from the command line like this:
-# python dna_analysis.py myfile.fastq
-
-
-###########################################################################
-### Libraries
-###
-
-# The sys module supports reading files, command-line arguments, etc.
 import sys
 
-
-###########################################################################
-### Read the nucleotides into a variable named seq
-###
-
-# You need to specify a file name
 if len(sys.argv) < 2:
     print "You must supply a file name as an argument when running this program."
     sys.exit(2)
 
-# The file name specified on the command line, as a string.
 filename = sys.argv[1]
-# A file object from which data can be read.
+
 inputfile = open(filename)
 
-# All the nucleotides in the input file that have been read so far.
 seq = ""
-# The current line number (= the number of lines read so far).
-linenum = 0
 
+linenum = 0
 
 for line in inputfile:
     linenum = linenum + 1
@@ -45,54 +26,61 @@ for line in inputfile:
         line = line.rstrip()
         seq = seq + line
 
-
+		
 ###########################################################################
 ### Compute statistics
 ###
 
-# Total nucleotides seen so far.
 total_count = 0
-# Number of G and C nucleotides seen so far.
 gc_count = 0
-
-# Number of nucleotides seen so far.
 at_count = 0
-g_count = 0
-c_count = 0
 a_count = 0
 t_count = 0
+g_count = 0
+c_count = 0
 
-
-# for each base pair in the string,
 for bp in seq:
-    # increment the total number of bps we've seen
     total_count = total_count + 1
+
+    if bp == 'C':
+        gc_count += 1
+        c_count += 1
+    elif bp == 'G':
+        gc_count += 1
+        g_count += 1
     
-    # next, if the bp is a G or a C,
-    if bp == 'C' or bp == 'G':
-        # increment the count of gc
-        gc_count = gc_count + 1
-        c_count = c_count + 1
-        g_count = g_count + 1
-    
-    # increment the count of at
-    if bp == 'A' or bp == 'T':
-        at_count = at_count + 1
-        a_count = a_count + 1
-        t_count = t_count + 1
-# divide the gc_count by the total_count
+    elif bp == 'A':
+        at_count += 1
+        a_count += 1 
+           
+    elif bp == 'T':
+        at_count += 1
+        t_count += 1
+
 gc_content = float(gc_count) / total_count
-
-# divide the at_count by the total_count
 at_content = float(at_count) / total_count
+at_gc_ratio = float(at_count) / (gc_count)
+a_content = float(a_count) / total_count
+c_content = float(c_count) / total_count
+g_content = float(g_count) / total_count
+t_content = float(t_count) / total_count
+sum_total = a_count + c_count + g_count + t_count
+seq_total = len(seq)
 
-
-# Print the answer
 print 'GC-content:', gc_content
 print 'AT-content:', at_content
-print 'G count:', g_count
-print 'C count:', c_count
-print 'A count:', a_count
-print 'T count:', t_count
+print 'A count:', a_content
+print 'T count:', t_content
+print 'G count:', g_content
+print 'C count:', c_content
+print 'Sum count:', sum_total
+print 'Total count:', total_count
+print 'Sequence Length:', seq_total
+print 'AT/GC ratio:', at_gc_ratio
 
-
+if gc_content > 0.6:
+    print 'GC Classification: High GC Content'
+elif gc_content < 0.4:
+    print 'GC Classification: Low GC Content'
+else:
+   print 'GC Classification: Moderate GC Content'
